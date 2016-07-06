f45c4b7ff035325afbdb0bc72dafff24ec794176
Jason Mittertreiner <jmittert@uwaterloo.ca>
2016-07-05 23:27:48 -0700

    Begin sql

--- car/car.go
+++ car/car.go
@@ -3,6 +3,8 @@ package main
 import (
   "fmt"
   "os"
+  "os/signal"
+  "syscall"
   "io"
   "net"
   "io/ioutil"
@@ -10,21 +12,55 @@ import (
   "flag"
   rc "github.com/jmittert/robotCar/lib"
   xbc "github.com/jmittert/xb360ctrl"
+  "database/sql"
+  _ "github.com/lib/pq"
 )
 
 type Config struct {
   ServerAddr string
+  DbAddr string
+  DbUser string
+  DbPass string
+  DbName string
 }
 
 var hw rc.HwState
+var db *sql.DB
+
+func cleanup(conn net.Conn, db *sql.DB, rc rc.HwState) {
+  if conn != nil {
+    conn.Close()
+  }
+  if db != nil {
+    db.Close()
+  }
+  rc.A1 = 0
+  rc.A2 = 0
+  rc.B1 = 0
+  rc.B2 = 0
+  rc.LPWM = 0
+  rc.RPWM = 0
+  rc.Write()
+}
+
 func main() {
   checkFlags()
   config := readConfig()
   serverAddr := config.ServerAddr
   bytes := make([]byte, 6)
 
+  var conn net.Conn
+  // Catch SIGTERM and clean up properly
+  c := make(chan os.Signal, 1)
+  signal.Notify(c, os.Interrupt)
+  signal.Notify(c, syscall.SIGTERM)
+  go func() {
+    <-c
+    cleanup(conn, db, hw)
+    os.Exit(0)
+  }()
   for {
-    conn := connect(serverAddr)
+    conn = connect(serverAddr)
     for {
       count, err := conn.Read(bytes)
       if count != 6 || err == io.EOF {
@@ -76,4 +112,15 @@ func connect(addr string) net.Conn {
     conn, err := net.DialTCP("tcp", nil, tcpAddr)
     checkError(err)
     return conn
+}
+
+func connectToDb(config Config) {
+  user := config.DbUser
+  addr := config.DbAddr
+  name := config.DbName
+  pass := config.DbName
+  var err error
+  db, err = sql.Open("postgresql",
+  fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=require", user, pass, addr, name))
+  checkError(err)
 }
